007032f89222c77d0a53ad9f12e5e0ef387bfdba
Chris Pitman <cpitman1@gmail.com>
2017-04-03 15:10:33 -0400

    Fix condition in script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 RESPONSE_TIME="$(curl -w '%{time_total}' -o /dev/null -s $TARGET_URL)"
-if ($?); then
-  curl -X POST -k -d "FAILED" $CALLBACK_URL
-else
+if [ $? -eq 0 ]; then
   curl -X POST -k -d $RESPONSE_TIME $CALLBACK_URL
+else
+  curl -X POST -k -d "FAILED" $CALLBACK_URL
 fi
